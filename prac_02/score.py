f9ffb8673f57765f0ff8806d6dd797c77307e34c
kelly <kelly.goosen@my.jcu.edu.au>
2026-02-07 13:20:47 +1000

    Add random score

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,21 +2,30 @@
 CP1404/CP5632 - Practical
 Program to determine score status
 """
+import random
+
 
 def main():
-    """Ask the user for their score and print the result."""
+    """Ask the user for their score and print the result, then show random score result."""
     score = float(input("Enter score: "))
     result = determine_result(score)
+    print(f"User score {score} is {result}")
+
+    random_score = random.uniform(0, 100)
+    random_result = determine_result(random_score)
+    print(f"Random: {random_score:.0f} = {random_result}")
 
 
 def determine_result(score):
+    """Return the result for a given score."""
     if score < 0 or score > 100:
-        print("Invalid score")
+        return "Invalid score"
     elif score >= 90:
-        print("Excellent")
+        return "Excellent"
     elif score >= 50:
-        print("Passable")
+        return "Passable"
     else:
-        print("Bad")
+        return "Bad"
+
 
 main()
